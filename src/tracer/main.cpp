defcc6e7493502d34b9c5d3c440d5ba89fc0c99f
Nikita Grigorev <pettyaelse@gmail.com>
2023-06-06 00:22:42 +0700

    wip

--- src/tracer/main.cpp
+++ src/tracer/main.cpp
@@ -3,8 +3,11 @@
 #include <unistd.h>
 
 #include <cassert>
+#include <fstream>
 #include <iostream>
-
+#include <istream>
+#include <sstream>
+#include <vector>
 
 using Regs = user_regs_struct;
 
@@ -27,17 +30,84 @@ union RegsUnion
 };
 
 
+struct Region
+{
+	size_t begin{};
+	size_t end{};
+};
+
+
+class Tracer
+{
+public:
+	Tracer(int pid)
+		: pid_{pid}
+	{
+		auto ret = ptrace(PTRACE_ATTACH, pid, 0, 0);
+		assert(ret == 0);
+	}
+
+	~Tracer()
+	{
+		ptrace(PTRACE_DETACH, pid_, 0, 0);
+	}
+
+	int getPid() const { return pid_; }
+
+private:
+	int pid_;
+};
+
 int main()
 {
 	int pid = 0;
+	std::cin >> pid;
+	Tracer tracer{pid};
 
-	long ret = -1;
-	do
+
+	std::string maps_filename = "/proc/";
+	maps_filename += std::to_string(pid);
+	maps_filename += "/maps";
+
+	std::ifstream maps_file{maps_filename};
+
+	if (!maps_file.is_open())
 	{
-		std::cin >> pid;
-		ret = ptrace(PTRACE_ATTACH, pid, 0, 0);
+		std::cout << "cannot open!" << std::endl;
 	}
-	while (ret != 0 && (std::cout << "error " << ret << std::endl));
+
+	std::vector<std::string> lines;
+
+	std::vector<Region> regions;
+
+	std::string cur_line;
+	while (std::getline(maps_file, cur_line))
+	{
+		if (cur_line.find("[heap]") != std::string::npos
+			|| cur_line.find("[stack]") != std::string::npos)
+		{
+			Region region;
+
+			std::stringstream sstr{cur_line};
+			sstr >> std::hex >> region.begin;
+			char spam;
+			sstr >> spam;
+			sstr >> std::hex >> region.end;
+
+
+			std::cout << "region " << std::hex << region.begin << " " << region.end << std::endl;
+			//			lines.push_back(cur_line);
+		}
+	}
+
+
+	//	std::string maps;
+	//	{
+	//		std::ostringstream sstr;
+	//		sstr << maps_file.rdbuf();
+	//		maps = sstr.str();
+	//	}
+
 
 	auto print_regs = [](const Regs &regs) {
 		RegsUnion u{};
@@ -49,19 +119,22 @@ int main()
 		}
 	};
 
-	while (true)
-	{
-		Regs r;
-		long regs_ret = ptrace(PTRACE_GETREGS, pid, NULL, &r);
-		if (regs_ret == 0)
-		{
-			print_regs(r);
-		}
-		else
-		{
-			std::cout << "error getregs " << regs_ret << std::endl;
-		}
-		std::cout << std::endl;
-	}
+
+	//	while (true)
+	//	{
+	//		Regs r;
+	//		long regs_ret = ptrace(PTRACE_GETREGS, pid, NULL, &r);
+	//		if (regs_ret == 0)
+	//		{
+	//			print_regs(r);
+	//		}
+	//		else
+	//		{
+	//			std::cout << "error getregs " << regs_ret << std::endl;
+	//		}
+	//		std::cout << std::endl;
+	//	}
+
+
 	return 0;
 }
