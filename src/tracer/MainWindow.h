9a5c87effc7ed7bb7df6d1a36d24e35083e7e4fa
Nikita Grigorev <pettyaelse@gmail.com>
2023-06-08 23:34:43 +0700

    wip

--- src/tracer/MainWindow.h
+++ src/tracer/MainWindow.h
@@ -2,6 +2,33 @@
 
 #include <QMainWindow>
 #include <QPushButton>
+#include <QVBoxLayout>
+
+
+
+class TracerWidget : public QWidget
+{
+public:
+	TracerWidget(QWidget *parent) : QWidget(parent)
+	{
+		new QVBoxLayout{this};
+
+
+		auto button = new QPushButton("Hello world!", this);
+		layout()->addWidget(button);
+		button->resize(200, 100);
+
+		auto button2 = new QPushButton("abababab", this);
+		layout()->addWidget(button2);
+		button2->resize(200, 100);
+
+
+	}
+
+
+};
+
+
 
 class MainWindow : public QMainWindow
 {
@@ -9,8 +36,11 @@ public:
 	MainWindow()
 		: QMainWindow()
 	{
-
-		auto button = new QPushButton("Hello world!", nullptr);
-		button->resize(200, 100);
+		tracer_widget_ = new TracerWidget{this};
+		setCentralWidget(tracer_widget_);
 	}
+
+
+private:
+	TracerWidget *tracer_widget_{};
 };
